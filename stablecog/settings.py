0dab534b68ae46b101cdb7c8d5bdc6484dec6d30
Hassan Kamran <125743684+LawlietReyuzaki@users.noreply.github.com>
2024-05-03 22:19:25 +0500

    Update settings.py

--- stablecog/settings.py
+++ stablecog/settings.py
@@ -16,6 +16,10 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
